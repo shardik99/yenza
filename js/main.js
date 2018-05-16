135db9fb82d989c59587ed8700bed6374c30439a
Hardik Singh <shardik99@gmail.com>
2018-05-16 12:18:31 +0530

    Created file structure for page

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var sideNav = document.querySelector('.sidenav');
+    M.Sidenav.init(sideNav, {});
+    // var elems = document.querySelectorAll('.dropdown-trigger');
+    // var instances = M.Dropdown.init(elems, {coverTrigger:false, constrainWidth:false, closeOnClick: false});
+});

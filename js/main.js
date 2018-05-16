f9ee4109e76b94b5349e413d12b4a6d4c866d825
Hardik Singh <shardik99@gmail.com>
2018-05-17 01:34:07 +0530

    Added header and service section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var sideNav = document.querySelector('.sidenav');
-    M.Sidenav.init(sideNav, {});
-    // var elems = document.querySelectorAll('.dropdown-trigger');
-    // var instances = M.Dropdown.init(elems, {coverTrigger:false, constrainWidth:false, closeOnClick: false});
+document.addEventListener("DOMContentLoaded", function() {
+  var sideNav = document.querySelector(".sidenav");
+  M.Sidenav.init(sideNav, {});
+  var par = document.querySelectorAll(".parallax");
+  M.Parallax.init(par, {});
+  var sSpy = document.querySelectorAll(".scrollspy");
+  M.ScrollSpy.init(sSpy, {});
+  // var elems = document.querySelectorAll('.dropdown-trigger');
+  // var instances = M.Dropdown.init(elems, {coverTrigger:false, constrainWidth:false, closeOnClick: false});
 });
